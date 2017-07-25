d77528b5f1bfc7a7a78e806085efa9b3c8bfb48e
pseguel <pseguel@cisco.com>
2017-07-24 23:33:41 -0400

    Envía correos correctamente en formato HTML

--- cumple_bot.py
+++ cumple_bot.py
@@ -9,6 +9,7 @@ import locale
 
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+from email.header import Header
 
 
 TOKEN = '4dlyvpcbi6lgm531ayg4zvma1t'
@@ -21,25 +22,28 @@ locale.setlocale(locale.LC_ALL, 'es_ES.UTF-8')
 
 column_map = {}
 
-def sendMail(receivers):
-    ts = time.time()
-    day = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d')
-    sender = 'no-reply@cisco.com'
+def sendMail(receivers, subject, text):
+    sender = '"Oficina Chile" <noreply@cisco.com>'
     host = 'mail.cisco.com'
     marker = "UNIQUEREPORTMARKER"
 
-    msg = MIMEMultipart('alternative')
-    body = "Reporte adjunto.\n\n"
+    msgRoot = MIMEMultipart('related')
+    msgRoot['Subject'] = Header(subject, 'utf-8')
+    msgRoot['From'] = sender
+    msgRoot.preamble = 'This is a multi-part message in MIME format.'
 
-    content = MIMEText(body, 'plain')
-    msg.attach(content)
-    msg['Subject'] = 'TEST Mail ' + day
-    msg['From'] = sender
+    msgAlternative = MIMEMultipart('alternative')
+    msgRoot.attach(msgAlternative)
 
+    msgText = MIMEText(text['plain'])
+    msgAlternative.attach(msgText)
+
+    msgText = MIMEText(text['html'], 'html')
+    msgAlternative.attach(msgText)
 
     try:
        smtpObj = smtplib.SMTP(host, 25)
-       smtpObj.sendmail(sender, receivers, msg.as_string())
+       smtpObj.sendmail(sender, receivers, msgRoot.as_string())
        print "Successfully sent email"
     except SMTPException:
        print "Error: unable to send email"
@@ -47,6 +51,7 @@ def sendMail(receivers):
 def cell_by_column_name(row, column_name):
     column_id = column_map[column_ame]
     return row.get_column(column_id)
+
 
 def eval_row(source_row, date =None):
     name = ''
@@ -76,17 +81,24 @@ def texto_mail(nombres, fecha):
         n = ", ".join(nombres[:-1]).encode('utf-8') + " y "+nombres[-1].encode('utf-8')
         v = "encuentran"
         plural = 's'
-    texto = "Team, les contamos que hoy {0} {1} de {2} se {3} de cumpleaños {4}.\n".format(w, d, m, v, n)
+    texto = "Team,\n\nles contamos que hoy {0} {1} de {2} se {3} de cumpleaños {4}.\n".format(w, d, m, v, n)
     texto+= "\n¡Le{0} deseamos muchísimas felicidades!".format(plural)
-    return texto
+
+    html_text = '<html><head><meta charset="utf-8"></head><body><p>Team,</p>'
+    html_text+= '<p>les contamos que hoy {0} {1} de {2} se {3} de cumpleaños '.format(w,d,m,v)
+    html_text+= '<b>{0}.</b></p>'.format(n)
+    html_text+= '<p>¡Le{0} deseamos muchísimas felicidades!</p>'.format(plural)
+    html_text+= '</body></html>'
+    return {'plain':texto, 'html':html_text}
+
 
 def subject_mail(nombres):
-    subject = '¡Feliz Cumpleaños '
+    subject = '¡¡¡Feliz Cumpleaños '
     if len(nombres) == 1:
         n = nombres[0].encode('utf-8')
     else:
         n = ", ".join(nombres[:-1]).encode('utf-8') + " y "+ nombres[-1].encode('utf-8')
-    subject += "{0}!".format(n)
+    subject += "{0}!!!".format(n)
     return subject
 
 ss = smartsheet.Smartsheet(TOKEN)
@@ -95,25 +107,26 @@ ss.errors_as_exceptions(True)
 sheet = ss.Sheets.get_sheet(SHEET_ID)
 
 
-
 for column in sheet.columns:
     column_map[column.title] = column.id
 
-#d = datetime.date(2017,6, 10)
-for i in range(1,366):
-    names = []
-    d = datetime.datetime.strptime('2017 {0}'.format(i), '%Y %j').date()
+#for i in range(1,366):
+names = []
+#d = datetime.datetime.strptime('2017 {0}'.format(i), '%Y %j').date()
+d = datetime.date(2017, 7, 26)
+#d = datetime.date.today()
 
-    for row in sheet.rows:
-        name = eval_row(row, d)
-        if name != '':
-            names.append(name)
+for row in sheet.rows:
+    name = eval_row(row, d)
+    if name != '':
+        names.append(name)
 
-    print names
+print names
 
-    if len(names)>0:
-        print subject_mail(names)
-        print texto_mail(names, d)
-
-    #sendMail(['pseguel@cisco.com']) 
+if len(names)>0:
+    subject = subject_mail(names)
+    print subject
+    texto = texto_mail(names, d)
+    print texto
+    sendMail(['pseguel@cisco.com'], subject, texto) 
 
