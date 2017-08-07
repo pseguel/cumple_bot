8c28eb5ac5879d9556fc4a2505453e793a11c198
pseguel <pseguel@cisco.com>
2017-08-06 22:36:27 -0400

    se genera imagen con mensaje, ésta incluye imagen de la gente

--- cumple_bot.py
+++ cumple_bot.py
@@ -3,19 +3,28 @@
 
 import os
 import smartsheet
+import requests
 import smtplib
 import time
 import datetime
 import locale
+import textwrap
 
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.header import Header
+from email.MIMEImage import MIMEImage
+
+import PIL
+from PIL import Image, ImageFilter, ImageFont, ImageDraw
+from StringIO import StringIO
 
 
 TOKEN = '4dlyvpcbi6lgm531ayg4zvma1t'
 SHEET_ID = int(os.environ['SHEET_ID'])
 # sheet_id = 7540004772702084
+SRC_IMAGE = 'media/cumple_background.png'
+font_color = (65,105,225) # royal blue
 
 locale.setlocale(locale.LC_ALL, 'es_ES.UTF-8')
 
@@ -56,6 +65,8 @@ def cell_by_column_name(row, column_name):
 
 def eval_row(source_row, date =None):
     name = ''
+    url = ''
+
     if date==None:
         date = datetime.date.today()
     this_month = str(date.month)
@@ -63,10 +74,14 @@ def eval_row(source_row, date =None):
 
     day_cell = source_row.get_column(COL_DAY)
     month_cell = source_row.get_column(COL_MONTH)
+    img_cell = source_row.get_column(COL_IMG)
 
     if day_cell.display_value == this_day and month_cell.display_value == this_month:
         name = source_row.get_column(COL_NAME).display_value
-    return name
+        urlImage = ss.models.ImageUrl()
+        urlImage.image_id  = source_row.get_column(COL_IMG).image.id
+        url = ss.Images.get_image_urls([urlImage]).image_urls[0].url
+    return name, url
 
 
 def texto_mail(nombres, fecha):
@@ -82,15 +97,16 @@ def texto_mail(nombres, fecha):
         n = ", ".join(nombres[:-1]).encode('utf-8') + " y "+nombres[-1].encode('utf-8')
         v = "encuentran"
         plural = 's'
-    texto = "Team,\n\nles contamos que hoy {0} {1} de {2} se {3} de cumpleaños {4}.\n".format(w, d, m, v, n)
+    texto = "Team,\n\nLes contamos que hoy {0} {1} de {2} se {3} de cumpleaños {4}.\n".format(w, d, m, v, n)
     texto+= "\n¡Le{0} deseamos muchísimas felicidades!".format(plural)
 
     html_text = '<html><head><meta charset="utf-8"></head><body><p>Team,</p>'
     html_text+= '<p>les contamos que hoy {0} {1} de {2} se {3} de cumpleaños '.format(w,d,m,v)
     html_text+= '<b>{0}.</b></p>'.format(n)
+    html_text+= '<p><imgi src="cid:image1"></p>'
     html_text+= '<p>¡Le{0} deseamos muchísimas felicidades!</p>'.format(plural)
     html_text+= '</body></html>'
-    return {'plain':texto, 'html':html_text}
+    return {'plain':texto,  'html':html_text}
 
 
 def subject_mail(nombres):
@@ -103,6 +119,50 @@ def subject_mail(nombres):
     return subject
 
 
+def resize_image(img, basewidth):
+    wpercent = (basewidth / float(img.size[0]))
+    hsize = int((float(img.size[1]) * float(wpercent)))
+    return img.resize((basewidth, hsize), PIL.Image.ANTIALIAS)
+
+
+def image_text(src_image, text, url_img_list):
+    font_size = 32
+    line_width = 40
+    im = Image.open(src_image)
+    img_faces = []
+    faces_x_offset = 300
+    faces_y_offset = 700
+    faces_x_step = 35
+    faces_x_size = 100
+    draw = ImageDraw.Draw(im)
+    font = ImageFont.truetype("CALIBRII.TTF", font_size)
+
+    for url_face in url_img_list:
+        print "*******"
+        print url_face
+        response = requests.get(url_face)
+        img_faces.append(Image.open(StringIO(response.content)))
+
+    lines = textwrap.wrap(text, width=line_width)#, replace_whitespace=False)
+    print text
+    print lines
+    line_height = font_size*2
+    x = 100
+    y = 400
+    for line in lines:
+        draw.text((x,y), unicode(line, 'utf-8'), font=font, fill=font_color)
+        y += font_size*1.5
+    n_face = 0
+    for face in img_faces:
+        offset = (faces_x_offset, faces_y_offset)
+        faces_x_offset += faces_x_size + faces_x_step
+        im.paste(resize_image(face, faces_x_size), offset)
+    im.save('cumple_img.png')
+
+
+def get_url_img_person(sheet):
+   pass
+
 ss = smartsheet.Smartsheet()
 ss.errors_as_exceptions(True)
 
@@ -114,20 +174,31 @@ for column in sheet.columns:
 COL_NAME = column_map['Nombre']
 COL_DAY = column_map['Dia']
 COL_MONTH = column_map['Mes']
-
+COL_IMG = column_map['Columna5'] #cambiar en smartsheet
 
 #for i in range(1,366):
 names = []
+people_url_img = []
 #d = datetime.datetime.strptime('2017 {0}'.format(i), '%Y %j').date()
-d = datetime.date(2017, 7, 26)
+d = datetime.date(2017, 6, 10)
 #d = datetime.date.today()
 
+correos = ['pseguel@cisco.com', 'lfaundez@cisco.com', 'lwannerp@cisco.com']
+correos = ['pseguel@cisco.com']
+#correos = ['oficina_chile@cisco.com']
+
 for row in sheet.rows:
-    name = eval_row(row, d)
+    name, url = eval_row(row, d)
     if name != '':
         names.append(name)
+        people_url_img.append(url)
 
 if len(names)>0:
     subject = subject_mail(names)
     texto = texto_mail(names, d)
-    sendMail(['pseguel@cisco.com'], subject, texto) 
+    print "after eval"
+    print names
+    print people_url_img
+    image_text(SRC_IMAGE, texto['plain'], people_url_img)
+    #sendMail(correos, subject, texto)
+
