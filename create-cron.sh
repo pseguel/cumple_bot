8f69bb237eee06b0610cbcfd972878fa3b8555e3
pseguel <pseguel@cisco.com>
2017-12-07 15:27:46 -0300

    added PROXY env

--- create-cron.sh
+++ create-cron.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-env | egrep 'BOT_PATH|SPARK_ACCESS_TOKEN|SMARTSHEET_ACCESS_TOKEN|ROOM_ID|SHEET_ID|MAIL_DEST' >> /etc/environment
-env | egrep 'BOT_PATH|SPARK_ACCESS_TOKEN|SMARTSHEET_ACCESS_TOKEN|ROOM_ID|SHEET_ID|MAIL_DEST' | cat - /tmp/crontab > /etc/cron.d/cumple-cron
+env | egrep 'PROXY|BOT_PATH|SPARK_ACCESS_TOKEN|SMARTSHEET_ACCESS_TOKEN|ROOM_ID|SHEET_ID|MAIL_DEST' >> /etc/environment
+env | egrep 'PROXY|BOT_PATH|SPARK_ACCESS_TOKEN|SMARTSHEET_ACCESS_TOKEN|ROOM_ID|SHEET_ID|MAIL_DEST' | cat - /tmp/crontab > /etc/cron.d/cumple-cron
 crontab /etc/cron.d/cumple-cron && cron -f
 #crond -L /var/log/cron/cron.log "$@" && tail -f /var/log/cron/cron.log
