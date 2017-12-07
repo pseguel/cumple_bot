656380f708cc07ba275f295942a6d4bd98fb56ba
pseguel <pseguel@cisco.com>
2017-12-07 09:00:38 -0300

    fix image cache issue in mobile devices (iOS)

--- cumple_bot.py
+++ cumple_bot.py
@@ -9,6 +9,7 @@ import time
 import datetime
 import locale
 import textwrap
+import uuid
 
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
@@ -27,7 +28,7 @@ SHEET_ID = int(os.environ['SHEET_ID'])
 # sheet_id = 7540004772702084
 BOT_PATH = os.environ['BOT_PATH']
 SRC_IMAGE = BOT_PATH+'media/images/cumple_background.png'
-OUT_IMAGE = 'cumple_img.png'
+OUT_IMAGE = str(uuid.uuid4())+'.png'
 ROOM_ID = os.environ['ROOM_ID']
 MAIL_DEST = os.environ['MAIL_DEST']
 font_color = (65,105,225) # royal blue
@@ -37,7 +38,10 @@ locale.setlocale(locale.LC_ALL, 'es_ES.UTF-8')
 column_map = {}
 
 
-def sendMail(receivers, subject, text, image_name=None):
+def sendMail(receivers, subject, text, image_name=None, content_id=None):
+    if content_id == None:
+        content_id = 'image1'
+
     sender = '"Oficina Chile" <noreply@cisco.com>'
     host = 'mail.cisco.com'
     marker = "UNIQUEREPORTMARKER"
@@ -58,7 +62,7 @@ def sendMail(receivers, subject, text, image_name=None):
 
     img = open(image_name,'rb').read()
     msgImg = MIMEImage(img, 'png')
-    msgImg.add_header('Content-ID', '<image1>')
+    msgImg.add_header('Content-ID', '<{0}>'.format(content_id))
     msgImg.add_header('Content-Disposition', 'inline', filename=image_name)
 
     msgRoot.attach(msgImg)
@@ -97,7 +101,10 @@ def eval_row(source_row, date =None):
     return name, url
 
 
-def texto_mail(nombres, fecha): 
+def texto_mail(nombres, fecha, content_id=None):
+    if content_id == None:
+        content_id = 'image1'
+
     line_width = 40
     d = fecha.day
     m = fecha.strftime('%B')
@@ -121,7 +128,7 @@ def texto_mail(nombres, fecha):
     html_text+= '<p>les contamos que hoy {0} {1} de {2} se {3} de cumpleaños '.format(w,d,m,v)
     html_text+= '<b>{0}.</b></p>'.format(n)
     html_text+= '<p>¡Le{0} deseamos muchísimas felicidades!</p>'.format(plural)
-    html_text+= '<p><img src="cid:image1"></p>'
+    html_text+= '<p><img src="cid:{0}"></p>'.format(content_id)
     html_text+= '</body></html>'
     return {'plain':texto,  'html':html_text}
 
@@ -174,7 +181,7 @@ def image_text(src_image, text, url_img_list):
     logo = resize_image(logo, 100)
     im.paste(logo, logo_offset, logo)
     im = resize_image(im, 400)
-    im.save(OUT_IMAGE)
+    im.save(OUT_IMAGE) 
 
 
 def get_url_img_person(sheet):
@@ -195,16 +202,20 @@ COL_DAY = column_map['Dia']
 COL_MONTH = column_map['Mes']
 COL_IMG = column_map['Columna5'] #cambiar en smartsheet
 
-#for i in range(1,366):
 names = []
 people_url_img = []
+# TEST CASES
+#for i in range(1,366):
 #d = datetime.datetime.strptime('2017 {0}'.format(i), '%Y %j').date()
+#d = datetime.date(2017, 3, 30)
+#d = datetime.date(2017, 5, 30)
 #d = datetime.date(2017, 8, 28)
+# END TEST CASES
 d = datetime.date.today()
 
 #correos = ['pseguel@cisco.com', 'lfaundez@cisco.com', 'lwannerp@cisco.com']
 #correos = ['oficina_chile@cisco.com']
-#correos = ['pseguel@cisco.com']
+#correos = ['pseguel@cisco.com', 'pmolling@cisco.com']
 correos = [MAIL_DEST]
 
 for row in sheet.rows:
@@ -214,10 +225,12 @@ for row in sheet.rows:
         people_url_img.append(url)
 
 if len(names)>0:
+    image_id = str(uuid.uuid4())
     subject = subject_mail(names)
-    texto = texto_mail(names, d)
+    texto = texto_mail(names, d, image_id)
+
     image_text(SRC_IMAGE, texto['plain'], people_url_img)
     #api.messages.create(roomId=ROOM_ID, text=texto['plain'], files=[OUT_IMAGE]) 
-    sendMail(correos, subject, texto, OUT_IMAGE)
+    sendMail(correos, subject, texto, OUT_IMAGE, image_id)
 else:
     print("No hay cumpleaños hoy")
